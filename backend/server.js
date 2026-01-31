8fd05e27b13437313166658fce4e7f366a774202
Keshav <ksckeshav12334@gmail.com>
2026-01-31 18:24:57 +0530

    Updated backend server listen and port

--- backend/server.js
+++ backend/server.js
@@ -91,4 +91,5 @@ app.get("/api/expiry-alerts", async(req,res)=>{
   res.json(meds);
 });
 
-app.listen(5000, ()=>console.log("Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
